8b49564f079e95aef3623732a59edc36b8919c42
Calvin304 <38512305+Calvin304@users.noreply.github.com>
2019-06-04 11:47:09 -0400

    removed reliance on python
    i found a nodejs package that does everything the python script did, but easier (and probably with better performance) without the whole child process stuff

--- statusbot.js
+++ statusbot.js
@@ -5,6 +5,7 @@ client.login(config.token);
 const spawn = require("child_process").spawn;
 const fs = require('fs');
 var FastMap = require("collections/fast-map.js");
+var mc = require('minecraft-protocol');
 
 
 let rawdata = fs.readFileSync('servers.json');  
@@ -15,6 +16,11 @@ function clean(text) {
     if (typeof(text) === "string")
       return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
     else return text;
+}
+
+function base64ToPNG(data) {
+	data = data.replace(/^data:image\/png;base64,/, '');
+	fs.writeFileSync('image.png', data, 'base64');
 }
 
 client.on("guildCreate", guild => {
@@ -288,46 +294,37 @@ client.on('message', message => {
 	
 	server = servers.guilds.get(message.guild.id).mcservers.find(server => server.commands.includes(command));
 	if (typeof server !== 'undefined') {
-		var process = spawn('python3',["./getstatus.py3",server.address.ip,server.address.port]);
-		process.stdout.on('data', (data) => {
-    	response = JSON.parse(data.toString());
-    	if (response.error == null) {
-		  	const embed = new Discord.RichEmbed()
-	  		.setTitle('Status of ' + server.name)
-	  		.setColor("00FF00")
-      	.setDescription("`" + response.description.text + "`\n")
-      	.addField("There " + ((response.players.online === 1)?"is **":"are **") + response.players.online + "/" + response.players.max +"** players online", ((response.players.online === 0)?"🙁":"`" + response.players.sample.map(player => player.name).join(", ") + "`"), true)
-      	;message.channel.send(embed);
-      	return;
-    	};
-    	if (response.error == "Connection Timed Out") {
+		mc.ping({host:server.address.ip,port:server.address.port},(err, pingResults) => {
+			if (err) {
 				const embed = new Discord.RichEmbed()
 		  	.setTitle('Status of ' + server.name)
 	  		.setColor("FF0000")
-	  		.setDescription("Connection Timed Out (server is offline)");
+	  		.setDescription(err);
   	    message.channel.send(embed);
 	      return;
-    	};
-    	if (response.error == "Connection Refused") {
-      	const embed = new Discord.RichEmbed()
+			}
+
+			if (pingResults.favicon !== undefined) {
+				base64ToPNG(pingResults.favicon);
+				const embed = new Discord.RichEmbed()
 	  		.setTitle('Status of ' + server.name)
-		  	.setColor("FF0000")
-		  	.setDescription("Connection Refused (either the server is online but the minecraft server isnt running or the port is wrong)");
-      	message.channel.send(embed);
-    	  return;
-  	  };
-	    if (response.error == "IOError") {
-      	const embed = new Discord.RichEmbed()
-	  		.setTitle('Status of ' + server.name)
-		  	.setColor("0000FF")
-		  	.setDescription("The Service running is not a Minecraft Server (check ip/url and port)");
-   	  	message.channel.send(embed);
-   	  	return;
-   	 }
-		});
-		process.stderr.on('data', (data) => {
-			console.log(data.toString());
-		});
-	}
+				.setColor("00FF00")
+				.attachFile({attachment:Buffer.from(pingResults.favicon.replace(/^data:image\/png;base64,/, ''), 'base64'),name:"image.png"})
+      	.setThumbnail("attachment://image.png")
+				.setDescription( (pingResults.description.text == "")?"":"`" + pingResults.description.text + "`")
+      .addField("There " + ((pingResults.players.online === 1)?"is **":"are **") + pingResults.players.online + "/" + pingResults.players.max +"** players online", ((pingResults.players.sample == undefined)?"🙁":"`" + pingResults.players.sample.map(player => player.name).join(", ") + "`"), true)
+      	;message.channel.send(embed);
+    		return;
+			};
+	  	const embed = new Discord.RichEmbed()
+	  	.setTitle('Status of ' + server.name)
+	  	.setColor("00FF00")
+			.setDescription( (pingResults.description.text == "")?"":"`" + pingResults.description.text + "`")
+      .addField("There " + ((pingResults.players.online === 1)?"is **":"are **") + pingResults.players.online + "/" + pingResults.players.max +"** players online", ((pingResults.players.sample == undefined)?"🙁":"`" + pingResults.players.sample.map(player => player.name).join(", ") + "`"), true)
+      ;message.channel.send(embed);
+    	return;
+    });
+    	
+	};
 	
 });
